8a049433ebb156be37e949ba892a463a9901c5bd
tranchin123 <dangvantranchinh@gmail.com>
2024-06-21 17:33:18 +0700

    config eslint and do examples for training js

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{
+		rules: {
+			semi: 'error',
+			'prefer-const': 'error',
+			'no-unused-vars': 'warn',
+			'no-console': 'off',
+			'func-names': 'off',
+			'no-process-exit': 'off',
+			'object-shorthand': 'off',
+			'class-methods-use-this': 'off',
+		},
+	},
+];
--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 var string = 'Dang Van Tran Van Chinh!';
 
 // Find index
-console.log(string.lastIndexOf('Van', 10));
+console.log(string.lastIndexOf('Van', 8));
 console.log(string.indexOf('Dung'));
 
 //Cut string
@@ -11,8 +11,8 @@ console.log(string.slice(0, 4));
 console.log(string.slice(-6, -1));
 
 // Replace
-console.log(string.replace('Dang', 'Nguyen'));
-console.log(string.replace(/Van/g, 'Văn'));
+console.log(string.replace('Dang', 'Nguyen')); // Nguyen Van Tran Van Chinh!
+console.log(string.replace(/Van/g, 'Văn')); // Dang Văn Tran Văn Chinh!
 
 //Convert to UpperCase
 console.log(string.toUpperCase());
@@ -21,8 +21,8 @@ console.log(string.toUpperCase());
 console.log(string.toLowerCase());
 
 // Trim (removes whitespace from both sides of a string)
-var myString = '   Hoc lap trinh javascript    ';
-console.log(myString.trim());
+const myString = '   Hoc lap trinh javascript    ';
+console.log(myString.trim()); // 'Hoc lap trinh javascript
 
 // Split (A string can be converted to an array )
 var languages = 'Javascript, Ruby, PHP';
@@ -38,20 +38,20 @@ console.log(students.toString());
 console.log(students.join(' - '));
 
 // Pop (removes the last element from an array and print element)
-console.log(students.pop());
-console.log(students);
+console.log(students.pop()); // Huy
+console.log(students); // ['Hung', 'John', 'Nhi']
 
 // Push (adds a new element to an array last index)
-console.log(students.push('Linh', 'Tien', 'Tuan'));
-console.log(students);
+console.log(students.push('Linh', 'Tien', 'Tuan')); // 6
+console.log(students); // ['Hung', 'John', 'Nhi', 'Linh', 'Tien', 'Tuan']
 
 // Shift (removes the first element from an array and print element)
-console.log(students.shift());
-console.log(students);
+console.log(students.shift()); //Hung
+console.log(students); //['John', 'Nhi', 'Linh', 'Tien', 'Tuan']
 
 // Unshift (adds a new element to an array first index)
-console.log(students.unshift('Chien', 'Hau'));
-console.log(students);
+console.log(students.unshift('Chien', 'Hau')); // 7
+console.log(students); // ['Chien', 'Hau', 'John', 'Nhi', 'Linh', 'Tien', 'Tuan']
 
 // Splicing
 var languages = ['javascript', 'PHP', 'Ruby'];
@@ -67,3 +67,330 @@ console.log(languages.concat(languages2));
 // slicing
 console.log(languages);
 console.log(languages.slice(0, 1));
+
+const Student = {
+	name: 'blue',
+	age: 16,
+};
+
+const className = 'className';
+Student[className] = 'F88';
+Student['gender'] = 'men';
+console.log(Student);
+
+const car = {
+	brand: {
+		name: 'Ford',
+	},
+	color: 'blue',
+};
+console.log(car.brand.name);
+console.log(car['brand']['name']);
+
+function User(firstName, lastName, age) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.age = age;
+}
+const author = new User('Chinh', 'Đặng', '22');
+const user = new User('Nhi', 'Hồ', '21');
+console.log(author);
+console.log(user);
+
+class Person {
+	hello() {
+		return 'Hello';
+	}
+}
+
+class Teacher extends Person {
+	hello() {
+		return super.hello() + ' I am student';
+	}
+}
+
+const a = new Teacher();
+console.log(a);
+console.log(a.hello());
+
+function doSomethingAsync(callback) {
+	setTimeout(function () {
+		const result = 'Dữ liệu từ tác vụ bất đồng bộ';
+		callback(result);
+	}, 6000);
+}
+
+function handleResult(data) {
+	console.log('Kết quả từ callback:', data);
+}
+
+doSomethingAsync(handleResult);
+
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map(function (num) {
+	return num * 3;
+});
+console.log(doubledNumbers);
+
+const courses = [
+	{
+		id: 1,
+		name: 'Javascript',
+		price: 200,
+	},
+	{
+		id: 2,
+		name: 'PHP',
+		price: 300,
+	},
+	{
+		id: 3,
+		name: 'Ruby',
+		price: 150,
+	},
+	{
+		id: 4,
+		name: 'React',
+		price: 0,
+	},
+	{
+		id: 5,
+		name: 'C++',
+		price: 0,
+	},
+	{
+		id: 6,
+		name: 'Ruby',
+		price: 330,
+	},
+];
+// forEach() Duyet qua cac phan tu trong mang
+courses.forEach(function (course, index) {
+	console.log(course, index);
+});
+
+// every() kiểm tra tất cả phần tử có thỏa mãn dieu kien (boolean)
+const isFree = courses.every(function (course) {
+	return course.price === 0;
+});
+console.log(isFree);
+
+// some() kiem tra chi can tu 1 phan tu thỏa mãn thì trả về true
+const c = courses.some(function (course) {
+	return course.price === 0;
+});
+console.log(c);
+
+// find() tim 1 phan tu co trong mang
+const course = courses.find(function (course) {
+	return course.name === 'PHP';
+});
+console.log(course);
+
+// filter() tim nhieu phan tu co trong mang
+const listCourse = courses.filter(function (course) {
+	return course.name === 'Ruby';
+});
+console.log(listCourse);
+
+const totalCoin = courses.reduce(function (a, b) {
+	return a + b.price;
+}, 0);
+console.log(totalCoin);
+
+var watchList = [
+	{
+		Title: 'Inception',
+		Year: '2010',
+		Rated: 'PG-13',
+		Released: '16 Jul 2010',
+		Runtime: '148 min',
+		Genre: 'Action, Adventure, Crime',
+		Director: 'Christopher Nolan',
+		Writer: 'Christopher Nolan',
+		Actors: 'Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy',
+		Plot: 'A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.',
+		Language: 'English, Japanese, French',
+		Country: 'USA, UK',
+		imdbRating: '8.8',
+		imdbVotes: '1,446,708',
+		imdbID: 'tt1375666',
+		Type: 'movie',
+	},
+	{
+		Title: 'Interstellar',
+		Year: '2014',
+		Rated: 'PG-13',
+		Released: '07 Nov 2014',
+		Runtime: '169 min',
+		Genre: 'Adventure, Drama, Sci-Fi',
+		Director: 'Christopher Nolan',
+		Writer: 'Jonathan Nolan, Christopher Nolan',
+		Actors: 'Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow',
+		Plot: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+		Language: 'English',
+		Country: 'USA, UK',
+		imdbRating: '8.6',
+		imdbVotes: '910,366',
+		imdbID: 'tt0816692',
+		Type: 'movie',
+	},
+	{
+		Title: 'The Dark Knight',
+		Year: '2008',
+		Rated: 'PG-13',
+		Released: '18 Jul 2008',
+		Runtime: '152 min',
+		Genre: 'Action, Adventure, Crime',
+		Director: 'Christopher Nolan',
+		Writer:
+			'Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)',
+		Actors: 'Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine',
+		Plot: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.',
+		Language: 'English, Mandarin',
+		Country: 'USA, UK',
+		imdbRating: '9.0',
+		imdbVotes: '1,652,832',
+		imdbID: 'tt0468569',
+		Type: 'movie',
+	},
+	{
+		Title: 'Batman Begins',
+		Year: '2005',
+		Rated: 'PG-13',
+		Released: '15 Jun 2005',
+		Runtime: '140 min',
+		Genre: 'Action, Adventure',
+		Director: 'Christopher Nolan',
+		Writer:
+			'Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)',
+		Actors: 'Christian Bale, Michael Caine, Liam Neeson, Katie Holmes',
+		Plot: 'After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.',
+		Language: 'English, Urdu, Mandarin',
+		Country: 'USA, UK',
+		imdbRating: '8.3',
+		imdbVotes: '972,584',
+		imdbID: 'tt0372784',
+		Type: 'movie',
+	},
+	{
+		Title: 'Avatar',
+		Year: '2009',
+		Rated: 'PG-13',
+		Released: '18 Dec 2009',
+		Runtime: '162 min',
+		Genre: 'Action, Adventure, Fantasy',
+		Director: 'James Cameron',
+		Writer: 'James Cameron',
+		Actors: 'Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang',
+		Plot: 'A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.',
+		Language: 'English, Spanish',
+		Country: 'USA, UK',
+		imdbRating: '7.9',
+		imdbVotes: '876,575',
+		imdbID: 'tt0499549',
+		Type: 'movie',
+	},
+];
+
+function calculateRating(array) {
+	const listMovie = array.filter(function (movie) {
+		return movie.Director === 'Christopher Nolan';
+	});
+
+	const totalImb = listMovie.reduce(function (total, movie) {
+		return total + parseFloat(movie.imdbRating);
+	}, 0);
+	return totalImb / listMovie.length;
+}
+// Expected results
+console.log(calculateRating(watchList)); // Output: 8.675
+
+function arrToObj(arr) {
+	return arr.reduce(function (obj, item) {
+		var key = item[0];
+		var value = item[1];
+		obj[key] = value;
+
+		return obj;
+	}, {});
+}
+
+var arr = [
+	['name', 'Sơn Đặng'],
+	['age', 18],
+];
+
+console.log(arrToObj(arr)); // { name: 'Sơn Đặng', age: 18 }
+
+// callback tao function myMap nhu ham map()
+Array.prototype.myMap = function (cb) {
+	const myArray = [];
+	for (const i in this) {
+		if (this.hasOwnProperty(i)) {
+			const result = cb(this[i], i);
+			myArray.push(result);
+		}
+	}
+	return myArray;
+};
+
+const numberss = [1, 2, 3];
+
+console.log(
+	numberss.myMap(function (number) {
+		return number * 2;
+	})
+); // Output: [2, 4, 6]
+
+console.log(
+	numberss.myMap(function (number, index) {
+		return number * index;
+	})
+); // Output: [0, 2, 6]
+
+Array.prototype.myFilter = function (cb) {
+	const output = [];
+	for (const i in this) {
+		if (this.hasOwnProperty(i)) {
+			const result = cb(this[i], i, this);
+			if (result) {
+				output.push(this[i]);
+			}
+		}
+	}
+	return output;
+};
+
+const myNumbers = [1, 2, 3, 4];
+
+console.log(
+	myNumbers.myFilter(function (number) {
+		return number % 2 === 0;
+	})
+);
+Output: [2, 4];
+
+console.log(
+	myNumbers.myFilter(function (number, index) {
+		return index % 2 === 0;
+	})
+);
+Output: [1, 3];
+
+console.log(
+	myNumbers.myFilter(function (number, index, array) {
+		return array.length % 2 === 0;
+	})
+);
+Output: [1, 2, 3, 4];
+
+const second = () => {
+	console.log('Hello there!');
+};
+const first = () => {
+	console.log('Hi there!');
+	second();
+	console.log('The End');
+};
+first();
