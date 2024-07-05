b34c337682e1fa00836347cd1d147dfcc173ccc6
tranchin123 <dangvantranchinh@gmail.com>
2024-07-05 16:28:25 +0700

    setup enviroment

---
+++ practice-one/eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{
+		rules: {
+			semi: 'error',
+			'prefer-const': 'warn',
+			'no-unused-vars': 'warn',
+			'no-console': 'off',
+			'func-names': 'off',
+			'no-process-exit': 'off',
+			'object-shorthand': 'off',
+			'class-methods-use-this': 'off',
+		},
+	},
+];
