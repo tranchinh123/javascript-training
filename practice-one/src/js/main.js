c820dd051e512aa5124bcbf9c81e6616d85d3594
tranchin123 <dangvantranchinh@gmail.com>
2024-07-10 14:45:52 +0700

    fix componenet card

--- practice-one/src/js/main.js
+++ practice-one/src/js/main.js
@@ -1,16 +1,5 @@
-fetch('https://5f7c244700bd74001690a4a7.mockapi.io/products', {
-	method: 'GET',
-	headers: { 'content-type': 'application/json' },
-})
-	.then((res) => {
-		if (res.ok) {
-			return res.json();
-		}
-		// handle error
-	})
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+const cardAdd = document.querySelector('.js-card-add');
+function showAddProduct() {
+	console.log(cardAdd);
+}
+cardAdd.addEventListener('click', showAddProduct);
