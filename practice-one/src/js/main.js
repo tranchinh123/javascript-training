9066947184f1b45fe0732fc6eab27a076808fe29
tranchin123 <dangvantranchinh@gmail.com>
2024-07-10 15:10:38 +0700

    hide and show modal add product

--- practice-one/src/js/main.js
+++ practice-one/src/js/main.js
@@ -1,5 +1,16 @@
-const cardAdd = document.querySelector('.js-card-add');
-function showAddProduct() {
-	console.log(cardAdd);
+const cardAdd = document.querySelector('.card-add');
+const modal = document.querySelector('.modal');
+const cancelBtn = document.querySelector('.btn-cancel');
+const modalContainer = document.querySelector('.modal-container');
+function showAddProductModal() {
+	modal.classList.add('open');
 }
-cardAdd.addEventListener('click', showAddProduct);
+function hideAddProductModal() {
+	modal.classList.remove('open');
+}
+cardAdd.addEventListener('click', showAddProductModal);
+cancelBtn.addEventListener('click', hideAddProductModal);
+modal.addEventListener('click', hideAddProductModal);
+modalContainer.addEventListener('click', (e) => {
+	e.stopPropagation();
+});
