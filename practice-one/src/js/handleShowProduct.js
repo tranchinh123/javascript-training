05409d38a8703a58be2757cc8121830d1b0e2e82
tranchin123 <dangvantranchinh@gmail.com>
2024-07-12 17:12:20 +0700

    rename file js

--- practice-one/src/js/handleShowProduct.js
+++ practice-one/src/js/handleShowProduct.js
@@ -1,20 +1,3 @@
-const cardAdd = document.querySelector('.card-add');
-const modal = document.querySelector('.modal');
-const cancelBtn = document.querySelector('.btn-cancel');
-const modalContainer = document.querySelector('.modal-container');
-function showAddProductModal() {
-	modal.classList.add('open');
-}
-function hideAddProductModal() {
-	modal.classList.remove('open');
-}
-cardAdd.addEventListener('click', showAddProductModal);
-cancelBtn.addEventListener('click', hideAddProductModal);
-modal.addEventListener('click', hideAddProductModal);
-modalContainer.addEventListener('click', (e) => {
-	e.stopPropagation();
-});
-
 function start() {
 	getProducts(renderFoods);
 }
