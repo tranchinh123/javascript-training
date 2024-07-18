f9d415c8d563203882825adf3cdc58e29af935bf
tranchinh123 <dangvantranchinh@gmail.com>
2024-07-19 00:36:58 +0700

    create validateform function

--- practice-one/src/js/main.js
+++ practice-one/src/js/main.js
@@ -5,6 +5,15 @@ const modal = document.querySelector('.modal');
 const cancelBtn = document.querySelector('.btn-cancel');
 const modalContainer = document.querySelector('.modal-container');
 const createButton = document.querySelector('.btn-save');
+
+const nameProduct = document.querySelector('input[name ="name-product"]');
+
+const price = document.querySelector('input[name ="price-product"]');
+
+const imgURL = document.querySelector('input[name ="img-product"]');
+
+const quantity = document.querySelector('input[name ="quantity-product"]');
+
 //...
 
 const showAddProductModal = () => {
@@ -46,21 +55,97 @@ const handleAddSuccess = (food) => {
 };
 
 // validate form
-// const validateForm = (formData) => {
-//     let isValid = false;
 
-//     if (formData.name === "") {
-//         isValid = false;
-//     }
-//     formData.name ok {
-//         isValid = true
-//     }
-//     //
+const showError = (input, message) => {
+	const parent = input.parentElement;
+	const formMessage = parent.querySelector('.form-message');
+	parent.classList.add('invalid');
+	formMessage.innerText = message;
+};
 
-//     return isValid
-// }
+const showSuccess = (input) => {
+	const parent = input.parentElement;
+	const formMessage = parent.querySelector('.form-message');
+	parent.classList.remove('invalid');
+	formMessage.innerText = '';
+};
 
-function handleAddProduct(e) {
+const checkEmptyError = (listInput) => {
+	let isEmptyError = false;
+	listInput.forEach((input) => {
+		input.value = input.value.trim();
+		if (!input.value) {
+			isEmptyError = true;
+			showError(input, ' The field cannot be empty.');
+		} else {
+			showSuccess(input);
+		}
+	});
+
+	return isEmptyError;
+};
+
+const checkImgUrlError = (input) => {
+	const regexImgUrl = new RegExp('(https?://.*.(?:png|jpg|jpeg|gif|png|svg))');
+	input.value = input.value.trim();
+	const isImgUrlError = !regexImgUrl.test(input.value);
+	if (regexImgUrl.test(input.value)) {
+		showSuccess(input);
+	} else {
+		showError(input, 'Image URL format is valid');
+	}
+
+	return isImgUrlError;
+};
+
+const checkIsNumberIntegerError = (input) => {
+	input.value = input.value.trim();
+	const numberInt = Number(input.value);
+	let isNumberIntError = false;
+	if (
+		typeof numberInt === 'number' &&
+		Number.isInteger(numberInt) &&
+		numberInt > 0
+	) {
+		isNumberIntError = false;
+		showSuccess(input);
+	} else {
+		isNumberIntError = true;
+		showError(
+			input,
+			'Please enter the number greater than 0 and number is integer '
+		);
+	}
+	return isNumberIntError;
+};
+
+const checkIsDecimalError = (input) => {
+	input.value = input.value.trim();
+	const numberDec = Number(input.value);
+	let isNumberDecimalError = false;
+	if (typeof numberDec === 'number' && !isNaN(numberDec) && numberDec > 0) {
+		isNumberDecimalError = false;
+		showSuccess(input);
+	} else {
+		isNumberDecimalError = true;
+		showError(input, 'Please enter the number greater than 0 ');
+	}
+	return isNumberDecimalError;
+};
+const validateForm = () => {
+	const isEmptyError = checkEmptyError([nameProduct, price, imgURL, quantity]);
+	const isImgUrlError = checkImgUrlError(imgURL);
+	const isNumberIntError = checkIsNumberIntegerError(quantity);
+	const isNumberDecError = checkIsDecimalError(price);
+
+	if (isEmptyError || isImgUrlError || isNumberIntError || isNumberDecError) {
+		return false;
+	} else {
+		return true;
+	}
+};
+
+const handleAddProduct = (e) => {
 	e.preventDefault();
 	const name = document.querySelector('input[name ="name-product"]').value;
 
@@ -78,21 +163,18 @@ function handleAddProduct(e) {
 		quantity: quantity,
 	};
 
-	createProduct(formData, handleAddSuccess);
-	// const isValid = validateForm(formData);
-
-	// if (isValid) {
-	//     createProduct(formData, handleAddSuccess);
-	// } else {
-
-	// }
-}
+	const isValid = validateForm();
+	if (isValid) {
+		createProduct(formData, handleAddSuccess);
+	} else {
+		console.log('Error');
+	}
+};
 
 const bindEvents = () => {
 	cardAdd.addEventListener('click', showAddProductModal);
 	cancelBtn.addEventListener('click', hideAddProductModal);
 	modal.addEventListener('click', hideAddProductModal);
-
 	createButton.addEventListener('click', handleAddProduct);
 	modalContainer.addEventListener('click', (e) => {
 		e.stopPropagation();
