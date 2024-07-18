71a3d6501f9a6c87ff959017bd39abec4666fc1f
tranchinh123 <dangvantranchinh@gmail.com>
2024-07-18 15:41:19 +0700

    create function handleAddsuccess and renderProductItem

--- practice-one/src/js/main.js
+++ practice-one/src/js/main.js
@@ -1,4 +1,3 @@
-// get list product
 // query DOM
 const productList = document.querySelector('.product-list');
 const cardAdd = document.querySelector('.card-add');
@@ -7,6 +6,98 @@ const cancelBtn = document.querySelector('.btn-cancel');
 const modalContainer = document.querySelector('.modal-container');
 const createButton = document.querySelector('.btn-save');
 //...
+
+const showAddProductModal = () => {
+	modal.classList.add('open');
+};
+const hideAddProductModal = () => {
+	modal.classList.remove('open');
+};
+
+const renderProductItem = (food) => {
+	return `
+            <div class="card card-product card-id-${food.id}">
+                                <div class="card-header">
+                                    <button class="icon-delete icon-id-${food.id}" onclick="handleDeleteProduct(${food.id})">&#9747;</button>
+                                </div>
+    
+                                <div class="card-main">
+                                    <img src="${food.image}" alt="default image" class="img-product" />
+                                    <div class="desc-product">
+                                        <p class="name-product">${food.name}</p>
+                                        <span class="price-product">$ ${food.price}</span>
+                                        <span class="dot">&#8226</span>
+                                        <span class="quantity-product">${food.quantity} bowls</span>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                        <img src="./assets/icons/Vector.svg" alt="">
+                                        <p class="text-edit">Edit dish</p>
+                                </div>
+                    </div>
+            `;
+};
+
+const handleAddSuccess = (food) => {
+	const newItem = renderProductItem(food);
+
+	productList.innerHTML += newItem;
+	hideAddProductModal();
+};
+
+// validate form
+// const validateForm = (formData) => {
+//     let isValid = false;
+
+//     if (formData.name === "") {
+//         isValid = false;
+//     }
+//     formData.name ok {
+//         isValid = true
+//     }
+//     //
+
+//     return isValid
+// }
+
+function handleAddProduct(e) {
+	e.preventDefault();
+	const name = document.querySelector('input[name ="name-product"]').value;
+
+	const price = document.querySelector('input[name ="price-product"]').value;
+
+	const imgURL = document.querySelector('input[name ="img-product"]').value;
+
+	const quantity = document.querySelector(
+		'input[name ="quantity-product"]'
+	).value;
+	const formData = {
+		name: name,
+		price: price,
+		image: imgURL,
+		quantity: quantity,
+	};
+
+	createProduct(formData, handleAddSuccess);
+	// const isValid = validateForm(formData);
+
+	// if (isValid) {
+	//     createProduct(formData, handleAddSuccess);
+	// } else {
+
+	// }
+}
+
+const bindEvents = () => {
+	cardAdd.addEventListener('click', showAddProductModal);
+	cancelBtn.addEventListener('click', hideAddProductModal);
+	modal.addEventListener('click', hideAddProductModal);
+
+	createButton.addEventListener('click', handleAddProduct);
+	modalContainer.addEventListener('click', (e) => {
+		e.stopPropagation();
+	});
+};
 
 //APIs => return product list
 const getProducts = async () => {
@@ -30,64 +121,7 @@ const getProducts = async () => {
 	}
 };
 
-const renderFoods = (foods) => {
-	const cardProduct = foods.map((food) => {
-		return `
-			<div class="card card-product card-id-${food.id}">
-								<div class="card-header">
-									<button class="icon-delete icon-id-${food.id}" onclick="handleDeleteProduct(${food.id})">&#9747;</button>
-								</div>
-	
-								<div class="card-main">
-									<img src="${food.image}" alt="default image" class="img-product" />
-									<div class="desc-product">
-										<p class="name-product">${food.name}</p>
-										<span class="price-product">$ ${food.price}</span>
-										<span class="dot">&#8226</span>
-										<span class="quantity-product">${food.quantity} bowls</span>
-									</div>
-								</div>
-								<div class="card-footer">
-										<img src="./assets/icons/Vector.svg" alt="">
-										<p class="text-edit">Edit dish</p>
-								</div>
-					</div>
-			`;
-	});
-	productList.innerHTML = cardProduct.join('');
-};
-
-// Handle show product list
-const loadProductList = async () => {
-	const products = await getProducts();
-	renderFoods(products);
-};
-
-const initApp = () => {
-	loadProductList();
-	handleAddProduct();
-};
-initApp();
-
-// show modal
-function handleModal() {
-	function showAddProductModal() {
-		modal.classList.add('open');
-	}
-	function hideAddProductModal() {
-		modal.classList.remove('open');
-	}
-
-	cardAdd.addEventListener('click', showAddProductModal);
-	cancelBtn.addEventListener('click', hideAddProductModal);
-	modal.addEventListener('click', hideAddProductModal);
-	modalContainer.addEventListener('click', (e) => {
-		e.stopPropagation();
-	});
-}
-handleModal();
-// call api => create
-const createProduct = async (data) => {
+const createProduct = async (data, callback) => {
 	try {
 		const response = await fetch(
 			'https://5f7c244700bd74001690a4a7.mockapi.io/products',
@@ -99,7 +133,7 @@ const createProduct = async (data) => {
 		);
 		if (response.ok) {
 			const data = await response.json();
-			return data;
+			return callback(data);
 		} else {
 			throw new Error('404');
 		}
@@ -107,30 +141,13 @@ const createProduct = async (data) => {
 		console.error('Something went wrong', error);
 	}
 };
-// handle add
-function handleAddProduct(id) {
-	createButton.addEventListener('click', (e) => {
-		e.preventDefault();
-		const name = document.querySelector('input[name ="name-product"]').value;
 
-		const price = document.querySelector('input[name ="price-product"]').value;
+const renderFoods = (foods) => {
+	const cardProduct = foods.map(renderProductItem);
 
-		const imgURL = document.querySelector('input[name ="img-product"]').value;
+	productList.innerHTML = cardProduct.join('');
+};
 
-		const quantity = document.querySelector(
-			'input[name ="quantity-product"]'
-		).value;
-		const formData = {
-			name: name,
-			price: price,
-			image: imgURL,
-			quantity: quantity,
-		};
-		createProduct(formData);
-		loadProductList();
-	});
-}
-// call api
 const handleDeleteProduct = async (id) => {
 	try {
 		const response = await fetch(
@@ -151,3 +168,16 @@ const handleDeleteProduct = async (id) => {
 		console.error('Something went wrong ', error);
 	}
 };
+
+// Handle show product list
+const loadProductList = async () => {
+	const products = await getProducts();
+	renderFoods(products);
+};
+
+const initApp = () => {
+	bindEvents();
+	loadProductList();
+	handleDeleteProduct();
+};
+initApp();
